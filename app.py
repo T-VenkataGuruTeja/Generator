93d52d19a399c6f9ff9e2a71fa9579c7276d24ed
T-VenkataGuruTeja <teja5056267@gmail.com>
2024-03-16 19:19:11 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import base64
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+home = tensorflow.keras.models.load_model('gen2.keras')
+
+bag = [
+    'avengers logo',
+    'nbkrist logo'
+]
+token = tensorflow.keras.preprocessing.text.Tokenizer(num_words = 10)
+token.fit_on_texts(bag)
+
+
+background = f"""
+<style>
+.stApp {{
+    background-image: url(data:image/{'png'};base64,{base64.b64encode(open('c.png', 'rb').read()).decode()});
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+}}
+
+.stTextInput {{
+    border: 2px solid crimson;
+    border-radius: 10px;
+    background: crimson;
+}}
+</style>
+"""
+
+st.markdown(background, unsafe_allow_html = True)
+
+col1, col2 = st.columns([0.7, 0.3])
+
+with col1:
+    st.markdown("<h3 style = 'color:red; margin-left: 0px;'>Image Generator</h3>", unsafe_allow_html = True)
+    text = st.text_input('Generate', placeholder = 'Enter Text Prompt................', label_visibility = 'collapsed')
+    submit = st.button('Generate')
+    clear = st.button('Clear')
+
+if submit:
+    noise = tensorflow.random.normal([1, 50], stddev = 0.2)
+    sequences = token.texts_to_sequences([text])
+    emb = tensorflow.convert_to_tensor([sequences])
+    img = home.predict([noise, emb])
+    img = img[0]
+    img = (img * 127.5) + 127.5
+    img = img.astype('uint8')
+    plt.imsave('img.jpg', img)
+    with col2:
+        st.image(img)
+    with open('img.jpg', 'rb') as file:
+        st.download_button('Download', data = file, file_name = 'image.jpg', mime = 'image/png')
+
+if clear:
+    with col2:
+        st.empty()
